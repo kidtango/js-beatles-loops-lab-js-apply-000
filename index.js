2d46ceeb9ca1eff4592c7423b53f87d9234c364d
kidtango <scott.q.tang@gmail.com>
2018-06-05 06:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,21 @@ function theBeatlesPlay(musicians, instruments) {
 
 console.log(theBeatlesPlay(musicians, instruments));
 
+const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
+
+  function johnLennonFacts(facts) {
+      let i = 0;
+      newFacts = [];
+      while (i < facts.length) {
+        newFacts.push(facts[i].concat("!!!"));
+        i++;
+      }
+      return newFacts;
+  }
+
+  console.log(johnLennonFacts(facts));
